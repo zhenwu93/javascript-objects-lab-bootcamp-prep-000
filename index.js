601a4c3fed019e86421f54d0342980786102b831
Zhen Wei Wu <zhenwu93@gmail.com>
2018-04-17 20:16:01 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue( object, key, value) {
+  return object
+  return object.assign({}, obj, { [key]: 'value'})
+}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return object
+  return object[key] = 'value';
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
