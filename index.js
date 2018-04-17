8dc04bfe06569a67e32d29937d39a0b4deba3980
Zhen Wei Wu <zhenwu93@gmail.com>
2018-04-17 20:34:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,3 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object
 }
-function deleteFromObjectByKey(object, key) {
-  delete object.key;
-  return object;
-}
