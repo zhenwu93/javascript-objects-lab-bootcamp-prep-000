a624fd3049f4974ed5ba20bd741fb486f69df4f7
Zhen Wei Wu <zhenwu93@gmail.com>
2018-04-17 20:25:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ function updateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object
 }
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value })
+}
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
